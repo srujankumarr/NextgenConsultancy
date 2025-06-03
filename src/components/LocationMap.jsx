faa36890689a531df14442ec6d2d4281c4e2f92b
sandeep <sandeep040949@gmail.com>
2025-06-03 18:40:01 +0530

    implemented fully responsive for all screens

--- src/components/LocationMap.jsx
+++ src/components/LocationMap.jsx
@@ -10,10 +10,11 @@ const LocationMap = () => {
             height="500"
             className="rounded-xl"
             id="gmap_canvas"
-            // change the location to your location
-            // Go to google maps, search for your location, click share, then embed map, copy the src link
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15557.626419613294!2d77.65282134901412!3d12.881555798807458!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1352a83559bf%3A0x24048c22b0d36caa!2sParappana%20Agrahara%2C%20Bengaluru%2C%20Karnataka%20560068!5e0!3m2!1sen!2sin!4v1741332207838!5m2!1sen!2sin"
-          ></iframe>
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3236.6354180141296!2d-78.94275902362589!3d35.68506337258645!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89ac941281c4cbe3%3A0xe573168f1e72dd03!2s3347%20Belterra%20Point%20Dr%2C%20New%20Hill%2C%20NC%2027562%2C%20USA!5e0!3m2!1sen!2sin!4v1717413231822!5m2!1sen!2sin"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          />
           <style>
             {`
             .mapouter {
